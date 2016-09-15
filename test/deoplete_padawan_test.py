f3fa1a2327cebf76663ed4c786d62c9dcadc1947
Pawel Bogut <pbogut@smeagol.pl>
2016-09-15 22:54:32 +0100

    Fix fuzzy match for namespaces

--- test/deoplete_padawan_test.py
+++ test/deoplete_padawan_test.py
@@ -49,8 +49,44 @@ class SourceTest(unittest.TestCase):
         #             '0....5....0.^ <- 12
         self.assertEqual(position, 12)
 
+    def test_get_complete_position_for_static_method_call_w_namespace(self):
+        position = self.source.get_complete_position(
+            {'input': '     \Some\Class::getInst'})
+        #             '0....5....0....5..^ <- 18
+        self.assertEqual(position, 18)
+
     def test_get_complete_position_for_fluent_method_calls(self):
         position = self.source.get_complete_position(
             {'input': '$db->select("*")->from("table")->whe'})
         #             '0....5....0....5....0....5....0..^ <- 33
         self.assertEqual(position, 33)
+
+    def test_get_complete_position_for_use_statement(self):
+        position = self.source.get_complete_position(
+            {'input': '  use Class\\'})
+        #             '0....5^ <- 6
+        self.assertEqual(position, 6)
+
+    def test_get_complete_position_for_long_use_statement(self):
+        position = self.source.get_complete_position(
+            {'input': '   use Class\With\Lon'})
+        #             '0....5.^ <- 7
+        self.assertEqual(position, 7)
+
+    def test_get_complete_position_for_new_statement(self):
+        position = self.source.get_complete_position(
+            {'input': ' $ala = new Class\\'})
+        #             '0....5....0.^ <- 12
+        self.assertEqual(position, 12)
+
+    def test_get_complete_position_for_long_new_statement(self):
+        position = self.source.get_complete_position(
+            {'input': ' $x = new Class\With\Ver'})
+        #             '0....5....^ <- 10
+        self.assertEqual(position, 10)
+
+    def test_get_complete_position_for_new_with_root_namespace(self):
+        position = self.source.get_complete_position(
+            {'input': '      new \Date'})
+        #             '0....5....0^ <- 11
+        self.assertEqual(position, 11)
